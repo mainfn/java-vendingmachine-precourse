2b9e4ba316bcc7b0a04ae9b5c44374e1eeef1714
mainfn <_@mainfn.dev>
2023-09-14 18:06:41 +0900

    feat: 랜덤 코인 생성 기능

--- src/main/java/vendingmachine/domain/coin/RandomCoinGenerator.java
+++ src/main/java/vendingmachine/domain/coin/RandomCoinGenerator.java
@@ -4,25 +4,27 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import vendingmachine.domain.money.Money;
 
 public final class RandomCoinGenerator {
 
-  private Coin generateCoin(final Money money) {
-    final int amount = Randoms.pickNumberInList(Arrays.asList(10, 50, 100, 500));
-    if (money.isEqualOrMoreThan(amount)) {
-      money.decrease(amount);
-      return Coin.of(amount);
-    }
-    return generateCoin(money);
+  private int pickAmountEqualsOrLessThan(final Money money) {
+    final List<Integer> amounts = Arrays.asList(10, 50, 100, 500)
+        .stream()
+        .filter(amount -> amount <= money.getAmount())
+        .collect(Collectors.toList());
+
+    return Randoms.pickNumberInList(amounts);
   }
 
   public Coins generate(final Money money) {
     final List<Coin> coins = new ArrayList<>();
 
-    while (money.isEqualOrMoreThan(0)) {
-      final Coin coin = generateCoin(money);
-      coins.add(coin);
+    while (money.isEqualOrMoreThan(10)) {
+      final int amount = pickAmountEqualsOrLessThan(money);
+      money.decrease(amount);
+      coins.add(Coin.of(amount));
     }
 
     return Coins.of(coins);
